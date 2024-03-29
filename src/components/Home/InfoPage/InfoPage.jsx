b3e8dae642de78bc94c5bb8a3989aa274e328552
Upayan Sarkar <142173510+UpayanSarkar@users.noreply.github.com>
2024-03-29 10:45:39 +0530

    Initial commit

---
+++ src/components/Home/InfoPage/InfoPage.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import './InfoPage.css';
+import { FaClock, FaHeadset,FaHouseUser  } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+const InfoPage = () => {
+    return (
+        <section className="why-us mt-5 mt-md-0">
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-lg-4 d-flex align-items-stretch">
+                        <div className="content">
+                            <h3>Why Choose Us?</h3>
+                            <p>
+                                Experience the synergy of innovative technology and personalized care,
+                                 designed to meet your holistic wellness needs. Benefit from the expertise of our 
+                                dedicated team and join our thriving community, where your well-being is our top priority.
+                                
+                            </p>
+                            <div className="text-center">
+                                <Link href="/" className="more-btn">Learn More <i className="bx bx-chevron-right"></i></Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-8 d-flex align-items-stretch">
+                        <div className="icon-boxes d-flex flex-column justify-content-center">
+                            <div className="row">
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaHouseUser className="icon"/>
+                                        <h4>Appointment</h4>
+                                        <small className='text-secondary'>24 Hours Service</small>
+                                        <p>Round-the-clock availability ensures you can book appointments anytime. 
+                                        Encounter efficient responses and adaptable options for seamless care. 
+                                        Trust in our emergency services for urgent medical needs.</p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaHeadset className="icon"/>
+                                        <h4>Emegency Cases</h4>
+                                        <h6 className='text-secondary'>+91 82406 07515</h6>
+                                        <p>In critical emergencies where immediate medical attention is required, please call to connect with our emergency response team,
+                                         ensuring swift assistance for urgent healthcare needs</p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaClock className="icon"/>
+                                        <h4>Working Hours</h4>
+                                        <small className='text-secondary'>Timing schedule</small>
+                                        <ul className='list-group list-group-flush'>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sun - Wed : </p> <p>8:00 - 17: 00</p></li>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Thus - Fri : </p> <p>9:00 - 17: 00</p></li>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sat - Sun : </p> <p>10:00 - 17: 00</p></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default InfoPage
