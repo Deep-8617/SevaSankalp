79c4c9759632f80b0715ceb2d4d04d56f8e31818
Tanmoy Mishra <124906627+Tanmoy-2001@users.noreply.github.com>
2024-05-21 23:38:43 +0530

    Chatbot Updated
    COMMIT BY GOD

--- src/components/Chatbot/Chatbot.js
+++ src/components/Chatbot/Chatbot.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComments, faPaperPlane, faTimes, faTrashAlt, faVolumeUp, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
+import { faComments, faPaperPlane, faTimes, faTrashAlt, faVolumeUp, faVolumeMute, faMicrophone, faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons';
 import stringSimilarity from 'string-similarity';
 import './Chatbot.css';
 
 const Chatbot = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isVoiceOn, setIsVoiceOn] = useState(true);
+    const [isListening, setIsListening] = useState(false);
+    const recognition = useRef(null);
     const initialMessage = { from: 'bot', text: 'Welcome to SevaSankalp! I am your SevaBot, How can I assist you today?' };
     const [messages, setMessages] = useState([initialMessage]);
     const messagesEndRef = useRef(null);
@@ -36,26 +38,43 @@ const Chatbot = () => {
         'what should i do in case of a medical emergency': 'In case of a medical emergency, please call us or go to our website to reach us.',
         'do you have specialists available': 'Yes, we have a range of specialists including cardiologists, dermatologists, and orthopedic surgeons.',
         'what are the common symptoms of Diabetes?': 'Common symptoms of diabetes may include increased thirst, frequent urination, fatigue, and blurred vision. However, it\'s essential to consult with a healthcare professional for an accurate diagnosis and treatment plan.',
-    'how can I manage Diabetes at home?': 'To manage diabetes at home, you can monitor your blood sugar levels regularly, follow a healthy diet, exercise regularly, and take prescribed medications as directed by your doctor. It\'s crucial to follow your healthcare provider\'s recommendations for optimal care.',
-    'what are the potential side effects of Metformin for Diabetes?': 'The potential side effects of Metformin for diabetes may include nausea, vomiting, diarrhea, and abdominal discomfort. Please consult with your healthcare provider for personalized advice and guidance.',
-    'what are the risk factors for Heart Disease?': 'Risk factors for heart disease may include high blood pressure, high cholesterol, smoking, obesity, and lack of physical activity. Understanding these factors can help you take proactive steps to reduce your risk.',
-    'how can I improve my cholesterol levels related to Heart Disease?': 'To improve cholesterol levels related to heart disease, you can follow a heart-healthy diet low in saturated and trans fats, exercise regularly, maintain a healthy weight, and avoid smoking. Additionally, consulting with a healthcare professional can provide personalized guidance.',
-    'what are the symptoms of high blood pressure?': 'Symptoms of high blood pressure may include headache, dizziness, blurred vision, chest pain, and shortness of breath. However, high blood pressure is often asymptomatic, so it\'s essential to monitor your blood pressure regularly.',
-    'how can I lower my blood pressure naturally?': 'To lower your blood pressure naturally, you can reduce sodium intake, eat a diet rich in fruits and vegetables, limit alcohol consumption, exercise regularly, and manage stress. However, it\'s essential to consult with a healthcare professional for personalized advice and guidance.',
-    'what are the signs of a stroke?': 'Signs of a stroke may include sudden numbness or weakness in the face, arm, or leg, especially on one side of the body, confusion, trouble speaking or understanding speech, trouble seeing in one or both eyes, and severe headache with no known cause. If you or someone else experiences these symptoms, seek immediate medical attention.',
-    'how can I prevent a stroke?': 'To prevent a stroke, you can maintain a healthy lifestyle by eating a balanced diet, exercising regularly, maintaining a healthy weight, not smoking, limiting alcohol consumption, and managing underlying health conditions such as high blood pressure, diabetes, and high cholesterol. Additionally, consult with your healthcare provider for personalized stroke prevention strategies.',
-    'what are the symptoms of depression?': 'Symptoms of depression may include persistent sadness, feelings of hopelessness or emptiness, loss of interest or pleasure in activities once enjoyed, changes in appetite or weight, difficulty sleeping or oversleeping, fatigue, feelings of worthlessness or guilt, difficulty concentrating or making decisions, and thoughts of death or suicide. If you or someone you know experiences these symptoms, seek help from a healthcare professional.',
-    'how can I support someone with depression?': 'To support someone with depression, you can listen to them without judgment, offer emotional support, encourage them to seek professional help, help them maintain a healthy lifestyle, and be patient and understanding. Additionally, educate yourself about depression and available treatment options.',
-    'who created you?': 'Upayan and Deep created me.'
-
+        'how can I manage Diabetes at home?': 'To manage diabetes at home, you can monitor your blood sugar levels regularly, follow a healthy diet, exercise regularly, and take prescribed medications as directed by your doctor. It\'s crucial to follow your healthcare provider\'s recommendations for optimal care.',
+        'what are the potential side effects of Metformin for Diabetes?': 'The potential side effects of Metformin for diabetes may include nausea, vomiting, diarrhea, and abdominal discomfort. Please consult with your healthcare provider for personalized advice and guidance.',
+        'what are the risk factors for Heart Disease?': 'Risk factors for heart disease may include high blood pressure, high cholesterol, smoking, obesity, and lack of physical activity. Understanding these factors can help you take proactive steps to reduce your risk.',
+        'how can I improve my cholesterol levels related to Heart Disease?': 'To improve cholesterol levels related to heart disease, you can follow a heart-healthy diet low in saturated and trans fats, exercise regularly, maintain a healthy weight, and avoid smoking. Additionally, consulting with a healthcare professional can provide personalized guidance.',
+        'what are the symptoms of high blood pressure?': 'Symptoms of high blood pressure may include headache, dizziness, blurred vision, chest pain, and shortness of breath. However, high blood pressure is often asymptomatic, so it\'s essential to monitor your blood pressure regularly.',
+        'how can I lower my blood pressure naturally?': 'To lower your blood pressure naturally, you can reduce sodium intake, eat a diet rich in fruits and vegetables, limit alcohol consumption, exercise regularly, and manage stress. However, it\'s essential to consult with a healthcare professional for personalized advice and guidance.',
+        'what are the signs of a stroke?': 'Signs of a stroke may include sudden numbness or weakness in the face, arm, or leg, especially on one side of the body, confusion, trouble speaking or understanding speech, trouble seeing in one or both eyes, and severe headache with no known cause. If you or someone else experiences these symptoms, seek immediate medical attention.',
+        'how can I prevent a stroke?': 'To prevent a stroke, you can maintain a healthy lifestyle by eating a balanced diet, exercising regularly, maintaining a healthy weight, not smoking, limiting alcohol consumption, and managing underlying health conditions such as high blood pressure, diabetes, and high cholesterol. Additionally, consult with your healthcare provider for personalized stroke prevention strategies.',
+        'what are the symptoms of depression?': 'Symptoms of depression may include persistent sadness, feelings of hopelessness or emptiness, loss of interest or pleasure in activities once enjoyed, changes in appetite or weight, difficulty sleeping or oversleeping, fatigue, feelings of worthlessness or guilt, difficulty concentrating or making decisions, and thoughts of death or suicide. If you or someone you know experiences these symptoms, seek help from a healthcare professional.',
+        'how can I support someone with depression?': 'To support someone with depression, you can listen to them without judgment, offer emotional support, encourage them to seek professional help, help them maintain a healthy lifestyle, and be patient and understanding. Additionally, educate yourself about depression and available treatment options.',
+        'who created you?': 'SevaSankalp Innovative Developers Team  Created me.',
+        'book': 'Choose your desired doctor as per your disease in the doctor section, otherwise go to our contact option and contact us here is the link - "http://localhost:3000/contact"',
+        'books': 'Choose your desired doctor as per your disease in the doctor section, otherwise go to our contact option and contact us here is the link - "http://localhost:3000/contact"',
+        'bokking': 'Choose your desired doctor as per your disease in the doctor section, otherwise go to our contact option and contact us here is the link - "http://localhost:3000/contact"',
+        'how to book': 'Choose your desired doctor as per your disease in the doctor section, otherwise go to our contact option and contact us here is the link - "http://localhost:3000/contact"',
+        'what are the doctor\'s timings': 'Please visit our doctor section for details or contact us here is the link -(http://localhost:3000/contact)',
+        'doctor timings': 'Please visit our doctor section for details or contact us here is the link -(http://localhost:3000/contact)',
+        'doctor availability': 'Please visit our doctor section for details or contact us here is the link -(http://localhost:3000/contact)',
+        'what time is the doctor available': 'Please visit our doctor section for details or contact us here is the link -(http://localhost:3000/contact)',
+        'time': 'Please visit our doctor section for details or contact us here is the link -(http://localhost:3000/contact)',
+        'why sevasankalp?': 'SevaSankalp is dedicated to providing top-quality healthcare with a compassionate touch. Our team of experienced professionals is here to support your health and well-being.',
+        'rating': 'Doctor ratings vary based on patient feedback and specialty. Please visit the doctor section on our website for detailed ratings and reviews.',
+        'approx booking rate': 'Our doctor booking prices vary depending on the doctor. Please visit the doctor section on our website for specific rates.',
+        'booking rate': 'Our doctor booking prices vary depending on the doctor. Please visit the doctor section on our website for specific rates.',
+        'price': 'Our doctor booking prices vary depending on the doctor. Please visit the doctor section on our website for specific rates.',
+        'how much does it cost to book a doctor?': 'The cost to book a doctor varies. Please visit the doctor section on our website for detailed pricing information.',
+        'doctor cost': 'The cost to book a doctor varies. Please visit the doctor section on our website for detailed pricing information.',
+        'how much is an appointment?': 'The cost of an appointment varies. Please visit the doctor section on our website for detailed pricing information.',
     };
 
     const quickReplies = [
-        { text: 'Payment', key: 'what payment methods do you accept' },
+        { text: 'Payment', key:  'what payment methods do you accept' },
         { text: 'Availability', key: 'what are your working hours' },
         { text: 'Services', key: 'what services do you offer' },
         { text: 'Appointment', key: 'how can i book an appointment' },
-        { text: 'Insurance', key: 'what insurance do you accept' }
+        { text: 'Insurance', key: 'what insurance do you accept' },
+        { text: 'Booking', key: 'book' } // Added quick reply button for Booking
     ];
 
     const handleToggle = () => {
@@ -121,6 +140,47 @@ const Chatbot = () => {
         }
     }, [messages]);
 
+    useEffect(() => {
+        if ('webkitSpeechRecognition' in window) {
+            recognition.current = new window.webkitSpeechRecognition();
+            recognition.current.continuous = false;
+            recognition.current.interimResults = false;
+            recognition.current.lang = 'en-IN';
+
+            recognition.current.onresult = (event) => {
+                const transcript = event.results[0][0].transcript.toLowerCase().trim();
+                sendMessage(transcript);
+            };
+
+            recognition.current.onerror = (event) => {
+                console.error('Speech recognition error:', event.error);
+                setIsListening(false);
+            };
+
+            recognition.current.onend = () => {
+                setIsListening(false);
+                setMessages((prevMessages) => [
+                    ...prevMessages,
+                    { from: 'bot', text: 'Voice recognition stopped. Please press the microphone button to start again.' }
+                ]);
+            };
+        }
+    }, []);
+
+    const handleVoiceInput = () => {
+        if (isListening) {
+            recognition.current.stop();
+            setIsListening(false);
+        } else {
+            recognition.current.start();
+            setIsListening(true);
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                { from: 'bot', text: 'Listening... please speak now.' }
+            ]);
+        }
+    };
+
     return (
         <div className={`chatbot ${isOpen ? 'open' : ''}`} style={{ opacity: '0.9' }} aria-live="polite">
             {!isOpen && (
@@ -157,6 +217,9 @@ const Chatbot = () => {
                         ))}
                     </div>
                     <div className="input-container">
+                        <button className="mic-button" onClick={handleVoiceInput} aria-label="Voice input">
+                            <FontAwesomeIcon icon={isListening ? faMicrophoneSlash : faMicrophone} />
+                        </button>
                         <input type="text" placeholder="Type your message..." onKeyDown={handleUserMessage} aria-label="Type your message here" />
                         <button className="send-button" onClick={handleUserMessage} aria-label="Send message">
                             <FontAwesomeIcon icon={faPaperPlane} />
